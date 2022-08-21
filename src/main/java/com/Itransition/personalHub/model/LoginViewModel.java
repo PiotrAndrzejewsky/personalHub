98959b06c83a490e1712773ff98e8a62bbc75a27
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-08-21 11:38:25 +0200

    feat: create login view model

---
+++ src/main/java/com/Itransition/personalHub/model/LoginViewModel.java
@@ -0,0 +1,23 @@
+package com.Itransition.personalHub.model;
+
+public class LoginViewModel {
+    private String mUsername;
+    private String mPassword;
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public void setUsername(String username) {
+        this.mUsername = username;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        this.mPassword = password;
+    }
+
+}
