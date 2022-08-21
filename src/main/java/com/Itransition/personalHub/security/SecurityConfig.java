054141419785c39bace646953340de4e1f9d21f4
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-08-21 11:38:15 +0200

    feat: create security configuration

---
+++ src/main/java/com/Itransition/personalHub/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.Itransition.personalHub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(new AuthenticationConfiguration()));
+        customAuthenticationFilter.setFilterProcessesUrl("/user/login");
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers(POST,"/user/login").permitAll()
+                .and()
+                .authorizeRequests().antMatchers(POST,"/user/save").permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(customAuthenticationFilter)
+                .authorizeRequests().anyRequest().authenticated();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
