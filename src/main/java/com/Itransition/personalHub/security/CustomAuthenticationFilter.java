cd0dd544fa49aa7ed785ce258889abdc51743524
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-08-21 11:37:54 +0200

    feat: create authentication filter

---
+++ src/main/java/com/Itransition/personalHub/security/CustomAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.Itransition.personalHub.security;
+
+import com.Itransition.personalHub.model.LoginViewModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager mAuthenticationManager;
+    public static final Algorithm ALGORITHM = Algorithm.HMAC256("token");
+    public static final int EXPIRATION_DATE = 300000;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.mAuthenticationManager = authenticationManager;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+        String access_token = createToken(user.getUsername(), EXPIRATION_DATE, request.getRequestURL().toString());
+        String refresh_token = createToken(user.getUsername(), EXPIRATION_DATE * 5, request.getRequestURL().toString());
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        LoginViewModel credentials = null;
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginViewModel.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword());
+        return mAuthenticationManager.authenticate(authenticationToken);
+    }
+
+    public static String createToken(String subject, int time, String url) {
+        return JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(new Date(System.currentTimeMillis() + time))
+                .withIssuer(url)
+                .sign(ALGORITHM);
+    }
+}
