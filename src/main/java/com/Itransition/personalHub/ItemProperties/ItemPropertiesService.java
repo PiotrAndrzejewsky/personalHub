a16de5a6ab613e287eedf9ec87b44020fbe3e765
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-09-04 13:23:28 +0200

    fix: change update item properties and add createItemProperties method

--- src/main/java/com/Itransition/personalHub/ItemProperties/ItemPropertiesService.java
+++ src/main/java/com/Itransition/personalHub/ItemProperties/ItemPropertiesService.java
@@ -4,4 +4,5 @@ public interface ItemPropertiesService {
     void updateItemProperties(ItemPropertiesEntity itemPropertiesEntity, Long itemId);
     ItemPropertiesEntity getItemPropertiesByItemId(Long itemId);
     void deleteItemPropertiesByItemId(Long itemId);
+    void createItemProperties(ItemPropertiesEntity itemPropertiesEntity, Long itemId);
 }
